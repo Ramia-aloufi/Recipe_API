df1c8e56c364946b5fbae844fdf3ffbb3881a0ce
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-10-02 15:14:47 +0300

    handle auth

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { updateUser, deleteUser, addUser, getAll } from '../controllers/user.controller';
+import { updateUser, deleteUser, addUser, getAll, getUserData } from '../controllers/user.controller';
 import { getById } from '../services/category.service';
 import { isAdmin, isLoggedIn } from '../middleware/auth.middleware';
 
@@ -8,6 +8,6 @@ export const userRouter = Router();
 userRouter.post('/', addUser);
 userRouter.get('/',isLoggedIn,isAdmin, getAll);
 userRouter.get('/:id',isLoggedIn, getById);
-userRouter.get('/profile',isLoggedIn, getById);
+userRouter.get('/profile',isLoggedIn, getUserData);
 userRouter.put('/:id',isLoggedIn, updateUser);
 userRouter.delete('/:id',isLoggedIn,isAdmin, deleteUser);
