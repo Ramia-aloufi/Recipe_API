81e95d0f05d967073f8e040953f5fbe9eb937dba
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-10-02 14:49:07 +0300

    handle auth

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -20,6 +20,8 @@ export const isLoggedIn = (req:CustomRequest,res:Response,next:NextFunction)=>{
   try{
   // const token = req.cookies[dev.AUTH_TOKEN];
   const token = req.headers['authorization']?.split(' ')[1];
+  console.log(token);
+  
   if(!token){
     throw createError(400, "Access denied. Please log in.")
   }
