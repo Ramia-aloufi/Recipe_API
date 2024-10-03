25c2bd23d7646cedb2608f147ad28f190fe45aca
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-10-03 09:58:41 +0300

    handle auth

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(cors( {
   credentials:true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Authorization' , 'Content-Type'],
-  exposedHeaders: ['Authorization'],
+  exposedHeaders: ['Authorization,X-Requested-With,Content-Type'],
   }
   ))
 
